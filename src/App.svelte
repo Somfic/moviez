6041baca88a831aae8225724cf3b3c6fe1477f7b
Lucas <somfic@gmail.com>
2022-07-17 15:46:30 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { default as ShowingComponent } from "./components/Showing.svelte";
+
+  import { fetchShowings } from "./scripts/api";
+  import { MediaType, Showing } from "./scripts/models/movies";
+
+  let showings: Showing[] | undefined;
+  let selectedShowing: Showing | undefined;
+
+  onMount(async () => {
+    showings = await fetchShowings();
+  });
+
+  function selected(showing: Showing | undefined) {
+    selectedShowing = showing;
+  }
+</script>
+
+<main>
+  {#if showings == undefined}
+    <div class="loading">
+      <p>Loading</p>
+    </div>
+  {:else}
+    {#each showings as showing}
+      <div class="background">
+        <img
+          src={"https://cdn.kinepolis.nl/images" +
+            showing.film.images.find((i) => i.mediaType == MediaType.Still).url}
+          class={selectedShowing == showing ? "active" : ""}
+          alt=""
+        />
+      </div>
+    {/each}
+    <div class="showings" on:mouseleave={() => selected(undefined)}>
+      {#each showings as showing}
+        <div class="showing" on:mouseenter={() => selected(showing)}>
+          <ShowingComponent {showing} />
+        </div>
+      {/each}
+    </div>
+  {/if}
+</main>
+
+<style lang="scss">
+  main {
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    .background {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+
+      img {
+        min-height: 100%;
+        min-width: 100%;
+
+        opacity: 0;
+        transition: 500ms ease;
+
+        &.active {
+          opacity: 1;
+        }
+      }
+    }
+
+    .showings {
+      display: flex;
+      align-items: center;
+      overflow-x: scroll;
+      padding: 2rem;
+      height: 100%;
+
+      .showing {
+        margin-right: -8rem;
+        transition: 200ms ease;
+
+        &:hover {
+          transform: translateY(-1rem);
+          padding-right: 1rem;
+        }
+
+        &:hover ~ .showing {
+          transform: translateX(8rem);
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components/Showing.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import { MediaType, Showing } from "../scripts/models/movies";
+  export let showing: Showing;
+  const cdn = "https://cdn.kinepolis.nl/images";
+</script>
+
+<div class="showing">
+  <p class="title">
+    <span class="name">{showing.film.title}</span>
+  </p>
+  <div class="poster">
+    <img
+      src={cdn +
+        showing.film.images.find((i) => i.mediaType == MediaType.PosterGraphic)
+          .url}
+      alt=""
+    />
+  </div>
+  <div class="info">
+    <div class="sessions">
+      {#each showing.sessions as session}
+        <p class="time info-item">
+          {session.showtime.getHours().toPrecision(2)}:{session.showtime
+            .getMinutes()
+            .toPrecision(2)}
+        </p>
+      {/each}
+    </div>
+    <p class="language info-item">{showing.film.audioLanguage}</p>
+  </div>
+</div>
+
+<style lang="scss">
+  @import "../styles/theme.scss";
+
+  .showing {
+    border: 1px solid $border;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    -webkit-backdrop-filter: saturate(180%) blur(20px);
+    backdrop-filter: saturate(180%) blur(20px);
+    background-color: transparentize($background-elevated, 0.28);
+    z-index: 10;
+    color: $foreground;
+    min-width: 380px;
+    box-shadow: -1rem 0 3rem transparentize($background, 0.7);
+
+    .title {
+      margin: 1rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      height: 1.5rem;
+
+      .name {
+        font-weight: bold;
+        font-size: 1.2rem;
+      }
+    }
+
+    .poster {
+      margin: 0rem 0.5rem;
+
+      img {
+        padding: 2px;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        overflow: hidden;
+        border-radius: 10px;
+      }
+    }
+
+    .info {
+      display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+      margin: 1rem;
+
+      .sessions {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+      }
+
+      .info-item {
+        background-color: transparentize($background, 0.5);
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+        color: $secondary;
+        //border: 1px solid $border;
+        font-size: 1rem;
+      }
+    }
+  }
+</style>
